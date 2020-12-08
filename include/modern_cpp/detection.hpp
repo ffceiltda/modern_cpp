3b0e43a26bc466d8db6816e59dfb3dc8901196e4
Virgilio A. Fornazin <virgilio.fornazin@fornazinconsultoria.com.br>
2020-12-08 10:19:26 -0300

    Fixed detection

--- include/modern_cpp/detection.hpp
+++ include/modern_cpp/detection.hpp
@@ -1,6 +1,10 @@
 #ifndef _MODERN_CPP_DETECTION_HPP_
 #define _MODERN_CPP_DETECTION_HPP_
 
+#ifndef _MODERN_CPP_PROLOG_HPP_
+#   error modern_cpp/detection.hpp: you must include <modern_cpp/prolog.hpp> before including this header
+#endif /* _MODERN_CPP_PROLOG_HPP_ */
+
 #include <modern_cpp/detection.h>
 
 #include <modern_cpp/detection/compiler.hpp>
--- include/modern_cpp/prolog.h
+++ include/modern_cpp/prolog.h
@@ -1,17 +1,13 @@
 #ifndef _MODERN_CPP_PROLOG_H_
 #define _MODERN_CPP_PROLOG_H_
 
-#ifdef __cplusplus
-#   include <modern_cpp/prolog.hpp>
-#endif /* __cplusplus */
-
 #include <modern_cpp/detection.h>
-
-#ifdef __cplusplus
-#   include <modern_cpp/detection.hpp>
-#endif /* __cplusplus */
-
 #include <modern_cpp/c_preprocessor.h>
 #include <modern_cpp/c_compatibility.h>
+
+#ifdef __cplusplus
+#   include <modern_cpp/prolog.hpp>
+#   include <modern_cpp/detection.hpp>
+#endif /* __cplusplus */
 
 #endif /* _MODERN_CPP_PROLOG_H_ */
